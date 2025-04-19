0becf649d9f943c329eeb136c5d163410bdcd8fd
shivaamaroju <naninaidu.0369@gmail.com>
2025-04-20 01:04:58 +0530

    Time: 43 ms (67.90%) | Memory: 56.6 MB (27.30%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,47 +1,63 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        Trie obj = new Trie();
-        for (var s : arr1) {
-            obj.insert(s + "");
-        }
-
-        int max = 0;
-        for (var s : arr2) {
-            max = Math.max(max, obj.search(s + ""));
-        }
-        return max;
+        Trie obj=new Trie();
+       for(var s:arr1){
+obj.insert(s+"");
+       } 
+       int max=0;
+       for(var s:arr2){
+       max=Math.max(max, obj.search(s+""));
+       }
+       return max;
     }
 }
-
 class Trie {
-    TNode root = new TNode();
-
-    public void insert(String word) {
-        TNode temp = root;
-        for (char ch : word.toCharArray()) {
-            int indx = ch - '0';
-            if (temp.a[indx] == null) {
-                temp.a[indx] = new TNode();
-            }
-            temp = temp.a[indx];
-        }
-        temp.ew = true;
+TNode root=new TNode();
+    public Trie() {
+        
     }
-
-    public int search(String word) {
-        TNode temp = root;
-        int c = 0;
-        for (char ch : word.toCharArray()) {
-            int idx = ch - '0';
-            if (temp.a[idx] == null) return c;
-            temp = temp.a[idx];
-            c++;
+    
+    public void insert(String word) {
+     TNode temp=root;
+        for(char ch:word.toCharArray()){
+            int indx=(int)ch-'0';
+            if(temp.a[indx]==null){
+            TNode nn=new TNode();
+            temp.a[indx]=nn;
+            temp=nn;    
+            }
+            else{
+                temp=temp.a[indx];
+            }
         }
+        temp.ew=true;
+    }
+    
+    public int  search(String word) {
+TNode temp=root;
+int c=0;
+for(char ch:word.toCharArray()){
+int idx=(int)ch-'0';
+if(temp.a[idx]==null)return c;
+temp=temp.a[idx];
+c++;
+}
+
         return c;
     }
+    
+    public boolean startsWith(String prefix) {
+        TNode temp=root;
+        for(char ch:prefix.toCharArray()){
+            int idx=(int)ch-'a';
+            if(temp.a[idx]==null)return false;
+            temp=temp.a[idx];
+        }
+        return true;
+    }
 }
 
-class TNode {
-    boolean ew = false;
-    TNode[] a = new TNode[10]; // fixed: digits 0-9
+class TNode{
+    boolean ew=false;
+    TNode a[]=new TNode[10];
 }
