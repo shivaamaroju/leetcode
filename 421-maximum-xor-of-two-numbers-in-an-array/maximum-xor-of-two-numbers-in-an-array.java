2e0813da22c2e1a35c8bca6d82ebdd33855feaef
shivaamaroju <naninaidu.0369@gmail.com>
2025-07-27 12:11:43 +0530

    Time: 836 ms (49.20%) | Memory: 185.1 MB (36.11%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,47 +1,49 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
-        Trie obj=new Trie();
+    Trie obj=new Trie();
         for(var s:nums){
             obj.insert(s);
         }
-        int max=0;
-        for(var s:nums){
-            max=Math.max(max,obj.maxor(s));
-        }
-    
-    return max;
-    }}
+int max=0;
+for(var s:nums){
+    max=Math.max(max,obj.maxor(s));
+}
+
+return max;
+
+    }
+}
 class Node{
     Node a[]=new Node[2];
 }
 class Trie{
- Node root=new Node();
- public void insert(int n){
-    Node temp=root;
+    Node root=new Node();
+    public void insert(int n){
+        Node temp=root;
     for(int i=31;i>=0;i--){
         int x=(n>>i)&1;
+        int y=1-x;
         if(temp.a[x]==null){
             temp.a[x]=new Node();
         }
         temp=temp.a[x];
-    }
- } 
- public int maxor(int n){
-    Node temp=root;
-    int max=0;
-    for(int i=31;i>=0;i--){
-      int x=(n>>i)&1;
-      int y=1-x;
-      if(temp.a[y]!=null){
-temp=temp.a[y];
-max|=(1<<i);
-      }
-      else if(temp.a[x]!=null){
-temp=temp.a[x];
-      }
-      else return max;
+    }}
 
+    public int maxor(int n){
+        int max=0;
+                Node temp=root;
+        for(int i=31;i>=0;i--){
+        int x=(n>>i)&1;
+        int y=1-x;
+        if(temp.a[y]!=null){
+            max|=(1<<i);
+            temp=temp.a[y];
+        }
+        else if(temp.a[x]!=null){
+            temp=temp.a[x];
+        }
+        else return max;
     }
     return max;
- }  
+    }
 }
